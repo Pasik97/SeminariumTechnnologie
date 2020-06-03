027937997f5cd87b1ceefb9bb1763ae17dc626d8
Pasik97 <k.pasik97@gmail.com>
2020-06-03 19:06:15 +0200

    Updates

---
+++ precache-manifest.c4eae03d9b92ff1804479081a9b47508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeea5383691d7db4871c45dbee1746c4",
+    "url": "/SeminariumTechnnologie/index.html"
+  },
+  {
+    "revision": "9536c2896eee6d165616",
+    "url": "/SeminariumTechnnologie/static/css/main.6fc04a3e.chunk.css"
+  },
+  {
+    "revision": "b28edd1ea0d9e689d80b",
+    "url": "/SeminariumTechnnologie/static/js/2.40c8737d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SeminariumTechnnologie/static/js/2.40c8737d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9536c2896eee6d165616",
+    "url": "/SeminariumTechnnologie/static/js/main.b98211cb.chunk.js"
+  },
+  {
+    "revision": "e5fa8203b1011829a38d",
+    "url": "/SeminariumTechnnologie/static/js/runtime-main.c2975b2b.js"
+  }
+]);
